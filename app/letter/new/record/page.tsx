884c1c9553772b54452003d87a908b93c3fc17c2
silverain <ekdldkaa@naver.com>
2026-01-27 17:35:27 +0900

    #3 style: add RecordNote Component

--- app/letter/new/record/page.tsx
+++ app/letter/new/record/page.tsx
@@ -1,7 +1,14 @@
+import RecordNote from '@/components/record/RecordNote';
+
 export default function RecordPage() {
   return (
-    <div>
-      <h1>Record Page</h1>
-    </div>
+    <article>
+      <section className='flex flex-col p-5 gap-4'>
+        <p className='typo-body1-base text-gray-900'>Tip! 바로 녹음하기 어렵다면, 초안 작성해보기</p>
+        <RecordNote/>
+      </section>
+
+      
+    </article>
   );
 }
