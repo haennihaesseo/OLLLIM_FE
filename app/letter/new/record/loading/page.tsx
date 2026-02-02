f4e8f8db5a4ca2117c3569000475b44800248f83
silverain <ekdldkaa@naver.com>
2026-02-02 11:28:56 +0900

    #14 feat: set loading page

--- app/letter/new/record/loading/page.tsx
+++ app/letter/new/record/loading/page.tsx
@@ -1,8 +1,14 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function RecordLoadingPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-full gap-4">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
-      <p className="text-gray-600 text-center">음성을 업로드하는 중입니다...</p>
+    <div className="flex flex-col items-center justify-center h-[80%] gap-4">
+      <p className="text-gray-400 text-center typo-h2-lg">
+        목소리에서
+        <br />
+        내용 추출 중...
+      </p>
+      <Spinner className="size-12 text-gray-400" />
     </div>
   );
 }
