f6f1b714d9119752216486bc8962a30b85632dc2
silverain <ekdldkaa@naver.com>
2026-02-06 16:35:08 +0900

    #30 chore: update kakaourl

--- hooks/auth/useKakaoLogin.ts
+++ hooks/auth/useKakaoLogin.ts
@@ -1,6 +1,6 @@
 export function useKakaoLogin(redirectUrl: string) {
   const handleKakaoLogin = () => {
-    window.location.href = `https://api.olllim.site/oauth2/authorization/kakao?redirect=${redirectUrl}`;
+    window.location.href = `https://server.olllim.site/oauth2/authorization/kakao?redirect=${redirectUrl}`;
   };
 
   return { handleKakaoLogin };
