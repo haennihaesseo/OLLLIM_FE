392b3b94fbb66f12b68d2aa3316091a9b7eb5511
silverain <ekdldkaa@naver.com>
2026-02-06 16:36:06 +0900

    #30 chore:server root dynamic

--- hooks/auth/useKakaoLogin.ts
+++ hooks/auth/useKakaoLogin.ts
@@ -1,6 +1,6 @@
 export function useKakaoLogin(redirectUrl: string) {
   const handleKakaoLogin = () => {
-    window.location.href = `https://server.olllim.site/oauth2/authorization/kakao?redirect=${redirectUrl}`;
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao?redirect=${redirectUrl}`;
   };
 
   return { handleKakaoLogin };
