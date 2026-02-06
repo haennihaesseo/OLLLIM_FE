4cc0c91c0a6c426e94e5b2d4075d6342a34857b6
silverain <ekdldkaa@naver.com>
2026-02-06 11:19:54 +0900

    #30 fix: post voice error

--- app/letter/new/record/page.tsx
+++ app/letter/new/record/page.tsx
@@ -1,8 +1,34 @@
+"use client";
+
 import RecordNote from "@/components/record/RecordNote";
 import VoiceRecorderContainer from "@/components/record/VoiceRecorderContainer";
 import CompleteButtonContainer from "@/components/record/CompleteButtonContainer";
+import { useAtomValue } from "jotai";
+import { audioBlobAtom } from "@/store/recordingAtoms";
+import { usePostLetterVoice } from "@/hooks/apis/post/usePostLetterVoice";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function RecordPage() {
+  const audioBlob = useAtomValue(audioBlobAtom);
+  const { mutate, isPending, isSuccess } = usePostLetterVoice();
+
+  const handleComplete = () => {
+    if (!audioBlob) return;
+    mutate(audioBlob);
+  };
+
+  if (isPending || isSuccess)
+    return (
+      <div className="flex flex-col items-center justify-center h-[80%] gap-4">
+        <p className="text-gray-400 text-center typo-h2-lg">
+          목소리에서
+          <br />
+          내용 추출 중...
+        </p>
+        <Spinner className="size-12 text-gray-400" />
+      </div>
+    );
+
   return (
     <article className="flex flex-col justify-between h-full">
       <div>
@@ -18,7 +44,7 @@ export default function RecordPage() {
         </section>
       </div>
 
-      <CompleteButtonContainer />
+      <CompleteButtonContainer onClick={handleComplete} disabled={isPending} />
     </article>
   );
 }
