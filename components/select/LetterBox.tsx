ee636945e071c8f2a20f4b5478e4f31e5c6eddce
silverain <ekdldkaa@naver.com>
2026-02-08 20:01:57 +0900

    #30 fix: letter box 레이아웃 미세 조정

--- components/select/LetterBox.tsx
+++ components/select/LetterBox.tsx
@@ -104,15 +104,15 @@ export default function LetterBox({
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-      className={`relative bg-white rounded-[0.5rem] py-10 px-5 h-[480px] flex flex-col ${
+      className={`relative bg-white rounded-[0.5rem] p-10 h-[490px] flex flex-col ${
         isEdit ? "border-2 border-dashed border-primary-700" : ""
       }`}
     >
-      <h3 className="mb-4">To. {title}</h3>
+      <h3 className="mb-5">To. {title}</h3>
 
       <div className="flex-1 overflow-y-auto">
         <p className="whitespace-pre-wrap">{renderContent()}</p>
-        <p className="text-gray-900 text-right mt-4 mb-5">From. {sender}</p>
+        <p className="text-gray-900 text-right mt-5">From. {sender}</p>
       </div>
     </div>
   );
