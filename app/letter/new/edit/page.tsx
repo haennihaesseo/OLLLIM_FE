c8f26187cf9d05291d1081988a04346de2b26939
silverain <ekdldkaa@naver.com>
2026-02-11 16:24:23 +0900

    #50 refactor(letter): edit 페이지 불필요한 use client 지시문 제거

--- app/letter/new/edit/page.tsx
+++ app/letter/new/edit/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Suspense } from "react";
 import PageLoading from "@/components/common/PageLoading";
 import { LetterEditContainer } from "@/components/edit/LetterEditContainer";
